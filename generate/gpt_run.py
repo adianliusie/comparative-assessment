e99c2932f1ccf10550c2bd373567018b63b614f7
Potsawee Manakul <m.potsawee@gmail.com>
2023-07-06 23:54:07 +0100

    refactor prompt template

--- generate/gpt_run.py
+++ generate/gpt_run.py
@@ -29,7 +29,7 @@ def main(
 
     #load prompt from default, or choose your own prompt
     #prompt_template = ...
-    prompt_template = get_default_template(prompt_num, ranking, score_type)
+    prompt_template = get_default_template(dataset, prompt_num, ranking, score_type)
 
     # get input text data to feed to chatgpt
     data_handler = DataHandler(prompt_template, dataset=dataset)
@@ -106,52 +106,27 @@ def add_arguments(parser):
     parser.add_argument('--ranking', type="bool", nargs="?", const=True, default=False)
     return parser
 
-def get_default_template(prompt_num, ranking, score_type):
+def get_default_template(dataset, prompt_num, ranking, score_type):
+    dirpath_ = os.path.dirname(__file__)
+    prompt_file = f"{dirpath_}/prompt_template/prompt_template{prompt_num}.txt"
+    with open(prompt_file, "r") as f:
+        prompt_template = f.read()
     if prompt_num == 1:
-        assert (ranking == True) and (score_type=='consistency')
-        prompt_template = "Assess the following two summaries given the corresponding article, and determine which passage is more consistent.\n\n<context>\n\nSummary A: <summary_1>\n\nSummary B: <summary_2>\n\nWhich Summary is more consistent relative to the passage, Summary A or Summary B?\n\nAnswer:"
+        assert (dataset == 'summeval') and (ranking == True) and (score_type=='consistency')
     elif prompt_num == 2:
-        assert (ranking == True) and (score_type=='consistency')
-        prompt_template = "Determine which of the two summaries is more consistent given the following article.\n\n<context>\n\nSummary A: <summary_1>\n\nSummary B: <summary_2>\n\nWhich Summary is more consistent relative to the passage, Summary A or Summary B?\n\nAnswer:"
+        assert (dataset == 'summeval') and (ranking == True) and (score_type=='consistency')
     elif prompt_num == 3:
-        assert (ranking == True) and (score_type=='consistency')
-        prompt_template = "Assess the following two summaries given the corresponding article, and determine which passage is more consistent. Note that consistency measures how much information included in the summary is present in the source article.\n\n<context>\n\nSummary A: <summary_1>\n\nSummary B: <summary_2>\n\nWhich Summary is more consistent relative to the passage, Summary A or Summary B?\n\nAnswer:"
+        assert (dataset == 'summeval') and (ranking == True) and (score_type=='consistency')
     elif prompt_num == 4:
-        assert (ranking == False) and (score_type=='consistency')
-        prompt_template = "Score the following summary given the corresponding article with respect to consistency from 1 to 10.\n\nSummary:<summary_1>\n\nSource Article:<context>\n\nMarks:"
+        assert (dataset == 'summeval') and (ranking == False) and (score_type=='consistency')
     elif prompt_num == 5:
-        assert (ranking == False) and (score_type=='consistency')
-        prompt_template = "Determine a score between 1 and 100 for how consistent the following summary is with respect to the article\n\nSource Article:<context>\n\nSummary:<summary_1>\n\nScore:"
+        assert (dataset == 'summeval') and (ranking == False) and (score_type=='consistency')
     elif prompt_num == 6:
-        assert (ranking == False) and (score_type=='consistency')
-        prompt_template = "Score the following summary given the corresponding article with respect to consistency from 1 to 10. Note that consistency measures how much information included in the summary is present in the source article. 10 points indicate the summary contains only statements that are entailed by the source document\n\nSummary:<summary_1>\n\nSource Article:<context>\n\nMarks:"
-
+        assert (dataset == 'summeval') and (ranking == False) and (score_type=='consistency')
     elif prompt_num == 7:
-        assert (ranking == False) and (score_type=='use_knowledge')
-        prompt_template = """Give a score from 1 to 10 of the following task. Output only a number between 1 and 10.
-
-Given the "fact" that the "response" of the "dialogue" is conditioned on, how well does the response use that fact?
-Note that a score of 10 points means the response uses the fact well, while a score of 1 means the response does not mention or refer to the fact at all
-
-fact: <fact>
-dialogue: <context>
-response: <response>
-
-Score:"""
-
+        assert (dataset == 'topicalchat') and (ranking == False) and (score_type=='use_knowledge')
     elif prompt_num == 8:
-        assert (ranking == True) and (score_type=='use_knowledge')
-        prompt_template = """Given the "fact" that a "response" of the "dialogue" is conditioned on, which of the two "response" are more grounded to the fact?
-Note that a grounded response means it uses the fact well, a not grounded response means it does not mention or refer to the fact at all
-Output only either "response A" or "response B"
-
-fact: <fact>
-dialogue: <context>
-response A: <response_A>
-response B: <response_B>
-
-Answer:"""
-
+        assert (dataset == 'topicalchat') and (ranking == True) and (score_type=='use_knowledge')
     return prompt_template
 
 if __name__ == "__main__":
